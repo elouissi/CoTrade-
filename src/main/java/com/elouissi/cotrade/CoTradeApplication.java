e27903e75668c1fa9d7536c24ec06ffc68b6ca87
elouissi <yassinelouissi67@gmai.com>
2024-12-05 17:17:34 +0100

    first commit

---
+++ src/main/java/com/elouissi/cotrade/CoTradeApplication.java
@@ -0,0 +1,13 @@
+package com.elouissi.cotrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoTradeApplication.class, args);
+    }
+
+}
